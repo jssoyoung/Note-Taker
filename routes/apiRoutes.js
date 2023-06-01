913a75d8f7fa66b36dac600075af07137ee4f1c2
Jessica Yun <jessicasopark@gmail.com>
2023-05-31 18:07:54 -0700

    Changed folder name

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
 
-router.get('/notes', (req, res) => {
+// GET Route for retrieving all the notes
+router.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+  
+  // GET Route for a specific note
+  router.get('/:note_id', (req, res) => {
+    const noteId = req.params.tip_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.note_id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  });
+
+router.post('/notes', (req, res) => {
     console.log(req.body);
 
     const { title, note } = req.body;
@@ -13,7 +36,7 @@ router.get('/notes', (req, res) => {
         note_id: uuidv4(),
       };
   
-      readAndAppend(newNote, '../db.json');
+      readAndAppend(newNote, './db.json');
       res.json(`Note added successfully 🚀`);
     } else {
       res.error('Error in adding note');
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const htmlRoutes = require('./routes/htmlRoutes')
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 
 const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 app.use(express.static('public'));
 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.listen(PORT, () =>
