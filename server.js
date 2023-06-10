4a0adbc4dfb7e96e714d21a4d4dff858360e4be4
Jessica Yun <jessicasopark@gmail.com>
2023-06-10 11:52:34 -0700

    Added comments to server.js

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// Uses express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Sets static website to contain code in public folder
 app.use(express.static('public'));
+
+// Links to the different routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+// Will display on console log 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
