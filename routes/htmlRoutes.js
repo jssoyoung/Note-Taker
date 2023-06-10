688cc74dd81b7c725ebecc59afeb633393d72568
Jessica Yun <jessicasopark@gmail.com>
2023-06-10 11:51:32 -0700

    Added comments to js file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const router = require('express').Router();
 
+// Route that if /notes is typed, notes.html will show
 router.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
+// Code that if anything else is typed after /, index.html will show
 router.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/index.html'))
 );
